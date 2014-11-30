b0d8bbd81f4766f6038585cf9fae57725778f7ab
Marko Elezovic <marko@element.hr>
2014-11-30 22:31:24 +0100

    Added Maven project. Updated dependencies Jackson Databinder / JodaTime, added slf4j-nop. Created local repository in lib/ + deploy scripts. Create deploy script to create assembly and publish jar to app folder.

---
+++ Benchmark/create-jar.bat
@@ -0,0 +1,8 @@
+echo off
+cd "%~dp0"
+
+call mvn clean compile package
+
+if exist target\json-benchmark-1.0.0-SNAPSHOT-jar-with-dependencies.jar (
+  copy target\json-benchmark-1.0.0-SNAPSHOT-jar-with-dependencies.jar ..\app\json-benchmark.jar
+)
